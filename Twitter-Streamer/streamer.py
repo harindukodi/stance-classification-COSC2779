b27ca63fcb4ac56c04f76761223e56f698391151
harindukodi <harindukodi@outlook.com>
2020-10-05 21:23:03 +1100

    implement simple query to stream

--- Twitter-Streamer/streamer.py
+++ Twitter-Streamer/streamer.py
@@ -1,1 +1,15 @@
-print('Hello World')
+from twitterscraper import query_tweets
+
+if __name__ == '__main__':
+    print('program started')
+    list_of_tweets = query_tweets("Trump OR Clinton", 10)
+
+    # print the retrieved tweets to the screen:
+    for tweet in query_tweets("Trump OR Clinton", 10):
+        print(tweet)
+
+    # Or save the retrieved tweets to file:
+    file = open('output.txt', 'w')
+    for tweet in query_tweets("Trump OR Clinton", 10):
+        file.write(str(tweet.text.encode('utf-8')))
+    file.close()
